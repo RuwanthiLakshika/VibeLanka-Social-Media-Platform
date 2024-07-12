bfefd0e4928840377a47b0c0973f4af7442598fc
RuwanthiLakshika <daruwanthilakshika@gmail.com>
2024-07-12 08:57:16 +0530

    changes related to left pane

--- fakebook/src/pages/Home/Home.jsx
+++ fakebook/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import NavBar from '../../components/Navigation/NavBar'
 import LeftPane from '../../components/LeftPane/LeftPane'
 import PostPane from '../../components/PostPane/PostPane'
 import RightPane from '../../components/RightPane/RightPane'
+import './home.css'
 
 export default function Home() {
   return (
