d528d352cd79f699ddd7c2e814b01457d11fcb44
RuwanthiLakshika <daruwanthilakshika@gmail.com>
2024-07-12 17:57:35 +0530

    changes related to post Top

---
+++ fakebook/src/components/Post/Post.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './post.css'
+
+export default function Post() {
+  return (
+    <div className='post'>
+        <div className="postContainer">
+            <div className="postTop">
+                <img src="./images/post1.jpg" alt="" className="postImage" />
+                <span className="postUserName">Ruwanthi Lakshika</span>
+                <span className="postTime">5 mins ago</span>
+            </div>
+            <div className="postCenter"></div>
+            <div className="postBottom"></div>
+        </div>
+    </div>
+  )
+}
