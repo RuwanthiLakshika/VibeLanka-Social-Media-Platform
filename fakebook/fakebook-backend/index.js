91a27c27c8266a2c6a693e3f4416206ea8d58d69
RuwanthiLakshika <daruwanthilakshika@gmail.com>
2024-07-18 09:30:09 +0530

    ad location to databse

--- fakebook/fakebook-backend/index.js
+++ fakebook/fakebook-backend/index.js
@@ -23,15 +23,16 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 app.post('/upload', upload.single('file'), async (req, res) => {
-  const { caption } = req.body;
+  const { caption,location } = req.body;
   try {
     const user = await prisma.user.create({
       data: { 
         image: req.file.filename,
         caption: caption,
+        location: location,
        },
     });
-    res.json({ user, postTime }); 
+    res.json(user); 
   } catch (err) {
     res.json(err);
   }
